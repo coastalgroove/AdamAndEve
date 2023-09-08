ede13bb63c491a83343ad4f7daaa9940c72c65f2
mangosax <mcguerty@att.net>
2023-09-08 07:53:32 -0500

    AdamAndEve

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace AdamAndEve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Create();
+        }
+
+        public static Human[] Create()
+        {
+            Human[] people = new Human[2];
+            Man adam = new Man();
+            Woman eve = new Woman();
+            people[0] = adam;
+            people[1] = eve;
+            return people;
+        }
+
+    }//class
+
+    public class Human
+    {
+    }
+    public class Man : Human
+    {
+    }
+    public class Woman : Human
+    {
+    }
+
+
+}//namespace
